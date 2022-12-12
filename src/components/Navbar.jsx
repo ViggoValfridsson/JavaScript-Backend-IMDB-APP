33e7b6ff8a02d5b3520f29af4b343777aefda526
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-12-12 12:24:39 +0100

    Created initial styling and a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import classes from "./Navbar.module.css";
+import { useRef, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { BiSearch } from "react-icons/bi";
+
+const Navbar = () => {
+  const searchRef = useRef();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!searchRef.current.value) {
+      navigate("/");
+    } else {
+      navigate(`/search/${searchRef.current.value}`);
+    }
+  };
+
+  return (
+    <header className={classes.header}>
+      <div className={`container ${classes.container__nav}`}>
+        <Link to="/">
+          <h1 className={classes.logo}>VMDb</h1>
+        </Link>
+        <nav className={classes.nav}>
+          <form onSubmit={handleSubmit}>
+            <div className={classes.searchInputGroup}>
+              <input type="text" ref={searchRef} className={classes.searchInput} placeholder="Search database" />
+              <button className={classes.iconButton}>
+                <BiSearch className={classes.icon} />
+              </button>
+            </div>
+          </form>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
