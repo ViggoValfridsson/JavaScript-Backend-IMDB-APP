0a13471f1964b22672e8a21818a59476d74b7d22
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-12-12 16:17:48 +0100

    Fixed styling on movie items

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,4 +1,4 @@
-import { AiOutlineStar } from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import classes from "./MovieItem.module.css";
 
@@ -15,16 +15,16 @@ const MovieItem = ({ movie }) => {
   console.log(movie);
   return (
     <Link to={"movie/" + movie.id} className={classes.movieCard}>
-        <div className={classes.image}>
-          <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt="" />
+      <div className={classes.image}>
+        <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt="" />
+      </div>
+      <div className={classes.content}>
+        <h3>{movieName}</h3>
+        <div className={classes.ratingContainer}>
+          <AiFillStar className={classes.icon} />
+          <h4 className={classes.ratingText}>{movie.vote_average.toFixed(1)}</h4>
         </div>
-        <div className={classes.content}>
-          <h3>{movieName}</h3>
-          <div className={classes.ratingContainer}>
-            <AiOutlineStar className={classes.icon} />
-            <h4 className={classes.ratingText}>{movie.vote_average.toFixed(1)}</h4>
-          </div>
-        </div>
+      </div>
     </Link>
   );
 };
