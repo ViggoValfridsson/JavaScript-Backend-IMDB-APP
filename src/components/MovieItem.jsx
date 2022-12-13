36d71bdf0d0dea5af1555d7d31bf57bd0bb110a7
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-12-13 09:27:36 +0100

    Fixed long titles sometimes overflowing

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { AiFillStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import classes from "./MovieItem.module.css";
@@ -12,18 +13,17 @@ const MovieItem = ({ movie }) => {
     movieName = movie.title;
   }
 
-  console.log(movie);
   return (
     <Link to={"movie/" + movie.id} className={classes.movieCard}>
       <div className={classes.image}>
         <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt="" />
       </div>
       <div className={classes.content}>
-        <h3>{movieName}</h3>
         <div className={classes.ratingContainer}>
           <AiFillStar className={classes.icon} />
           <h4 className={classes.ratingText}>{movie.vote_average.toFixed(1)}</h4>
         </div>
+        <h3>{movieName}</h3>
       </div>
     </Link>
   );
