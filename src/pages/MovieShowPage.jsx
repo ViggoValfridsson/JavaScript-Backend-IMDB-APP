d23d543fd6f2e5234edc31dfde3704b476082030
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-12-14 14:31:33 +0100

    Removed unnecessary attributes from iframe

--- src/pages/MovieShowPage.jsx
+++ src/pages/MovieShowPage.jsx
@@ -123,7 +123,6 @@ const MovieShowPage = () => {
                 className={classes.trailer}
                 src={getTrailerLink()}
                 title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
               ></iframe>
             </div>
