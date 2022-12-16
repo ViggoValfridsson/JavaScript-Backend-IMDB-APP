00ee0e1acc97d9aeaa7061c62a8c20f5fe9b4445
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-12-16 11:12:32 +0100

    Added logic to fetch data to movie items in dropdown

--- src/components/DropdownItem.jsx
+++ src/components/DropdownItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import classes from "./DropDownItem.module.css";
+import image_not_found from "../images/image_not_found.png";
 
 const DropdownItem = ({ movie }) => {
   const getMovieName = () => {
@@ -20,7 +21,21 @@ const DropdownItem = ({ movie }) => {
 
   return (
     <Link to={`/media/movie/${movie.id}`}>
-      <article>{getMovieName()}</article>
+      <article>
+        <div className={classes.image}>
+          {movie.poster_path && (
+            <img
+              src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`}
+              alt={"Poster for " + getMovieName()}
+            />
+          )}
+          {!movie.poster_path && <img src={image_not_found} alt={"Could not find poster for " + getMovieName()} />}
+        </div>
+        <div className={classes.content}>
+          <h3>{getMovieName()}</h3>
+          <p className={classes.description}>{movie.overview}</p>
+        </div>
+      </article>
     </Link>
   );
 };
